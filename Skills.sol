273393439034f852576ea816fdaa163d12590bd0
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2023-05-03 18:19:50 -0300

    Update Skills.sol

--- Skills.sol
+++ Skills.sol
@@ -5,12 +5,31 @@ import "./MelvorIdle.sol";
 
 contract Skills is MelvorIdle {
 
-    mapping(uint => bool) woodcutting;
+    mapping(uint => bool) public woodcutting;
+    mapping(uint => uint) public timeWoodcutting;
+
+    uint requiredToLevel2 = 0.1 hours;
+    uint requiredToLevel3 = 1 hours;
+    uint requiredToLevel4 = 10 hours;
+    uint requiredToLevel5 = 3 days;
+    uint requiredToLevel6 = 7 days;
+    uint requiredToLevel7 = 30 days;
 
     modifier ownerId(uint _id) {
         require(msg.sender ==  characterToOwner[_id]);
         _;
     }
+    function stopLastedSkillFarm(uint _id) external ownerId(_id) {
+        if (woodcutting[_id] == true){
+            woodcutting[_id] = false;
+        }
+    }
     
-    function _trainWoodcutting(uint _id) external ownerId(_id){}
+    function _trainWoodcutting(uint _id) external ownerId(_id){
+        stopLastedSkillFarm(_id);
+        woodcutting[_id] = true;
+        timeWoodcutting[_id] = block.timestamp;
+    }
+
+    
 }
