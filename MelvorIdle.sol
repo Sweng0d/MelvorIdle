ef8d7618eacf30c26b99463a08a3f2eaf660ebf7
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2023-05-03 13:14:25 -0300

    Update MelvorIdle.sol

--- MelvorIdle.sol
+++ MelvorIdle.sol
@@ -5,16 +5,19 @@ pragma solidity 0.8.17;
 contract MelvorIdle {
     
     Character[] public character;
-    Classes public class;
+    Classes internal class;
     uint public initialGold = 0;
     uint public startLevel = 1;
     uint public maxLevel = 99;
 
     mapping(address => uint) myCharacters;
 
+    event CharacterCreated(address indexed owner);
+
     struct Character {
         uint id;
         string name;
+        address owner;
         Classes class;
         uint gold;
         uint levelWoodcutting;
@@ -29,8 +32,10 @@ contract MelvorIdle {
     }
 
     function _createCharacter(string memory _name, Classes _class) public payable {
-        character.push(Character(character.length - 1, _name, _class, initialGold, startLevel, maxLevel));
+        //require(msg.value == 1 ether);
+        character.push(Character(character.length - 1, _name, msg.sender, _class, initialGold, startLevel, startLevel));
         myCharacters[msg.sender] = character.length -1;
+        //emit CharacterCreated(msg.sender);
     }
 
 }
